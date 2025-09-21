038e70e610da1a1b1940e1273317d1b5baab112e
Christian Otieno <iamchristianotieno@gmail.com>
2025-09-21 23:01:00 +0100

    Define core data structures and types
    - Add NavigationEvent struct for incoming event data
    - Add VisitorStats struct for API response format
    - Include JSON tags for proper serialization

---
+++ types.go
@@ -0,0 +1,11 @@
+package main
+
+type NavigationEvent struct {
+	VisitorID string `json:"visitor_id"`
+	URL       string `json:"url"`
+}
+
+type VisitorStats struct {
+	URL              string `json:"url"`
+	DistinctVisitors int    `json:"distinct_visitors"`
+}
